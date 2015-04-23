684c71b090df2619d54697d0e271d57b177dcb45
Paul Dowling <pdowling@nd.edu>
2015-04-23 02:55:51 -0400

    made a basic battle screen with no sprites yet.

--- graphic_implemented/Battle.h
+++ graphic_implemented/Battle.h
@@ -2,6 +2,35 @@
 //Battle.h
 //header file for the Battle screen
 
+#ifndef BATTLE_H
+#define BATTLE_H
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
 
+class Battle {
+	public:
+		Battle(); //costructor
+		~Battle(); //deconstructor call when the battle ends?
+		void choose_pokemon(); //select a pokemon
+		void fight(); //select attack move
+		void evolve(); //show increased stats
+		void set_battle_clips();
+		void update();
 
+
+}
+
+	private:
+		SDL_Surface *Battle_Background;  //Sprite clipping that has it store background
+		SDL_Surface *User_poke; //Sprite for user pokemon
+		SDL_Surface *enemy; //Sprite 
+		SDL_Rect Battle_clip[11]; //clips for sprite shhet
+
+}
+
+#endif
